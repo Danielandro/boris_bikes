606c336eef47e805f973f246ada273e94c1a3a20
Student <student@MAKERSs-MacBook-Pro-5.local>
2019-05-30 16:44:53 +0100

    Using Instance Variables

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,18 @@
 require '/Users/student/Desktop/Desktop/projects/boris_bikes/lib/bike.rb'
 
 class DockingStation
+attr_reader :docked_bike 
+
+  def initialize
+    @docked_bike = []
+  end
+
+  def dock(bike)
+    @docked_bike << bike
+  end
+
   def release_bike
     Bike.new
   end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,6 +3,7 @@ require 'docking_station'
 describe DockingStation do 
   station = DockingStation.new
 
+
   it 'is available as described class' do 
     expect(described_class).to eq(DockingStation)
   end
@@ -12,4 +13,14 @@ describe DockingStation do
   it 'returns a bike instance when #release_bike is called' do    
     expect(station.release_bike).to be_instance_of(Bike)
   end
+
+  it 'docking station should store a bike instance as attribute reader' do
+    bike = station.release_bike
+    expect(station.dock(bike)).to include(be_instance_of(Bike))
+  end
+
+  it 'return the stored bikes when attribute get called' do
+    expect(station.docked_bike).to include(be_instance_of(Bike))
+  end
+
 end

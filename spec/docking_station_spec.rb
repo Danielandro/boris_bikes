e9546d9bbf5568e0e8259e42b41b12a2b2ce67d7
Aleksandra <ajmpawlik@gmail.com>
2019-05-31 17:25:59 +0100

    passing feature test AMAZING VICTORY test that docking station gets bike and expects bike to be working

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,16 +1,23 @@
 require 'docking_station'
 
-describe DockingStation do 
+describe DockingStation do
   station = DockingStation.new
 
-  it 'is available as described class' do 
+  it 'is available as described class' do
     expect(described_class).to eq(DockingStation)
   end
 
   it { should respond_to :release_bike }
-  
-  it 'returns a bike instance when #release_bike is called' do    
-    expect(station.release_bike).to be_instance_of(Bike)
+
+  it 'gets a bike and the bike is working' do 
+    docking_station = DockingStation.new
+    bike = docking_station.release_bike
+    expect(bike.working?).to eq(true)
   end
+
+
+  # it 'returns a bike instance when #release_bike is called' do
+  #   expect(station.release_bike).to be_instance_of(Bike)
+  # end
 
 end
